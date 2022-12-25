8b46daaae4858786d4a4e911ce961435faf6d817
Radmir Ahmetgaliev <radmir.ahmetgaliev@gmail.com>
2022-12-25 20:46:23 +0500

    add rate students in class reviewer

--- OOP_HomeWork.py
+++ OOP_HomeWork.py
@@ -13,6 +13,19 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
         
+    # def rate_hw(self, student, course, grade):
+    #     if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+    #         if course in student.grades:
+    #             student.grades[course] += [grade]
+    #         else:
+    #             student.grades[course] = [grade]
+    #     else:
+    #         return 'Ошибка'
+            
+class Lecturer(Mentor):
+    pass
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -21,21 +34,15 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-            
-class Lecturer(Mentor):
-    pass
-
-class Reviewer(Mentor):
-    pass
  
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
  
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
  
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
  
 print(best_student.grades)
